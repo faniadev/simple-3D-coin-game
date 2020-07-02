185e955845413e1e479ca67e20f0ec5637181b0c
fania devira <fania_devira@yahoo.com>
2020-07-02 22:15:29 +0700

    simple-3D-coin-game

---
+++ Assets/Rotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(30, 90, 30) * Time.deltaTime); 
+    }
+}
